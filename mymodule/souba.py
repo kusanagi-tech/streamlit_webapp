4b49f08a0c352ed7cd361d1e78ccb439045a0aea
kusanagi <65207089+kusanagi-tech@users.noreply.github.com>
2024-06-25 11:49:12 +0900

    market report 6/25 renewed

--- mymodule/souba.py
+++ mymodule/souba.py
@@ -1,18 +1,18 @@
 comment = """
-メキシコペソは急上昇中で、戻りの相場に入っています。流石に急落しても7円台では買えませんでした。ドルが160円近くまで上昇しています。このためもらえるドルのスワップも5円ほど増えています。日銀の為替介入が再びくるのでしょうか？  
-◆円相場1ドル＝159円台に　為替介入あった4月末以来 約2か月ぶりの円安水準  
-https://youtu.be/2VY9COOXM1c?si=8QR15mVmqtXlC9lV  
-◆コメの先物取引 大阪の取引所の申請 政府認可 8月新市場開設へ  
-https://youtu.be/PJzcnPp0sR4?si=yoe46LQ2Y-Z5xyOc  
-◆【｢S&P500｣｢NYダウ｣｢ナスダック｣って結局なにが違うの？】  
-https://youtu.be/-yVbJVszc84?si=qF233djsz7UoAkor
+円相場1ドル160円迫る  
+https://youtu.be/xj6lqgk8fts?si=hpPYBLnrF5SUeJku    
+鈴木財務大臣「行き過ぎた動きには適切な対応」  
+https://youtu.be/TW_BLgh15SA?si=p8Kp8Kj3XyDHWvQL  
+ペソルマン回復か？！  
+https://youtu.be/cpGACdcI9-s?si=IYuJ1fJbcDvI_QCz  
+暗号資産は全体的に価格が下がっています。
 """
-date="6/24"
+date="6/25"
 #第一口座
 mswap1 =21*25
-usswap =24.2
-ruikei1 =207855
-saeki1 =282472
+usswap =24.4
+ruikei1 =208405
+saeki1 =296558
 hyouka1 = ruikei1 + saeki1
 yukou1 = (353009 + hyouka1)/149128
 
@@ -20,15 +20,15 @@ yukou1 = (353009 + hyouka1)/149128
 mswap2 =2*26
 leverage=4.13
 tswap =79*37
-ruikei2 =219324
-saeki2 =14250
+ruikei2 =222299
+saeki2 =2850
 hyouka2 = ruikei2 + saeki2
 yukou2 = (755349 + hyouka2)/166000
 
 #投資信託
-nikkei =38490
-bull =15509
-bear =873
+nikkei =38850
+bull =15749
+bear =861
 soneki = bull + bear - 15807
 
 #為替レートの自動取得
